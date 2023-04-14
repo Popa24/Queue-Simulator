b776e672587bc950d08be26e1dc8b44aae4ca93a
Alex Popa <62101412+Popa24@users.noreply.github.com>
2023-04-14 23:08:18 +0300

    Update queue.java for tommorrow

--- threads/src/main/java/org/example/model/Queue.java
+++ threads/src/main/java/org/example/model/Queue.java
@@ -24,6 +24,7 @@ public class Queue {
     public synchronized void updateServiceTime() {
         if (!clients.isEmpty()) {
             Client frontClient = clients.get(0);
+            //i think that it should be + not minus but will test tommow evening
             frontClient.serviceTime -= 1;
             if (frontClient.serviceTime == 0) {
                 clients.remove(0);
